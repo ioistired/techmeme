6001c86cb24513f4e874fa25fbf967a358d680a6
bmintz <bmintz@users.noreply.github.com>
2017-03-16 12:02:16 -0500

    add config parsing code

--- config.py
+++ config.py
@@ -14,3 +14,11 @@ class Config:
 	def __init__(self, video_filename, filename):
 		self.video_filename = video_filename
 		self.filename = filename
+		
+		self.parse_config()
+	
+	def parse_config(self):
+		with open(self.filename) as config_file:
+			# get first line and save as speed multiplier
+			self.multiplier = float(next(config_file).strip())
+			self.timestamps = [float(line.strip()) for line in config_file]
