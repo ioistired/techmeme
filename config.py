fc614a3c9b4bc8c47b377aef07b9b2815aa96341
bmintz <bmintz@users.noreply.github.com>
2017-03-16 12:08:48 -0500

    sort the timestamps

--- config.py
+++ config.py
@@ -22,3 +22,7 @@ class TechnicalMemeConfig:
 			# get first line and save as speed multiplier
 			self.multiplier = float(next(config_file).strip())
 			self.timestamps = [float(line.strip()) for line in config_file]
+			
+			# if the timestamps are not in order,
+			# the speedups will be applied in the wrong order
+			self.timestamps.sort()
