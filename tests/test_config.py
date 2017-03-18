6eba4cbd93712cf64d0f5c8cb909d27741fface1
bmintz <bmintz@users.noreply.github.com>
2017-03-17 22:07:11 -0500

    test_config.py: add tests for parsed attributes

--- tests/test_config.py
+++ tests/test_config.py
@@ -21,7 +21,17 @@ class TechnicalMemeConfigTests(unittest.TestCase):
 	def test_basic_attributes(self):
 		"""test the attributes that don't need to be parsed first"""
 		self.assertEqual(self.config.filename, 'tests/sample_config.txt')
-
+	
+	def test_parsed_attributes(self):
+		"""test the attributes that do need to be parsed"""
+		self.assertEqual(self.config.multiplier, 1.05)
+		self.assertEqual(self.config.timestamps, [
+			1.4142135623730951,
+			3.141592653589793,
+			5.0,
+			26.3,
+			69.420,
+		])
 
 if __name__ == '__main__':
 	import sys
