3ae76b80ef73e92b7ff14cef55b64ed6a3dab1a5
bmintz <bmintz@users.noreply.github.com>
2017-03-19 21:15:02 -0500

    TechnicalMeme: add get subclip and a basic init function

--- techmeme/techmeme.py
+++ techmeme/techmeme.py
@@ -10,11 +10,25 @@ techmeme.py: CLI app to turn videos into dank technical may-mays
 """
 
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video.fx.speedx import speedx
 
 from config import TechnicalMemeConfig
 
 
 class TechnicalMeme(CompositeVideoClip):
 	def __init__(self, *, source_filename, config):
-		self.source_filename = source_filename
+		self.source_video = VideoFileClip(source_filename)
 		self.config = config
+		self._fix_up_timestamps()
+	
+	def _fix_up_timestamps(self):
+		self.config.timestamps = [0] + self.config.timestamps + [self.source_video.end]
+	
+	def _get_subclip(self, timestamp_number):
+		"""get a subclip at the timestamp given by self.config.timestamps[timestamp_number]"""
+		
+		if timestamp_number < len(self.timestamps):
+			return self.source_video.subclip(*self.timestamps[timestamp_number:timestamp_number+2])
+		else:
+			raise ValueError("timestamp number too large")
