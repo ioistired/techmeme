42149025c0a03ab2159f8617fd24109250bb1dd9
bmintz @ GitHub <bmintz@users.noreply.github.com>
2017-03-19 19:08:21 -0500

    more changes lol

--- techmeme/idle_session.py
+++ techmeme/idle_session.py
@@ -2,13 +2,14 @@
 
 from moviepy.editor import *
 from techmeme.config import TechnicalMemeConfig
+
+config = TechnicalMemeConfig("tests/sample_config.txt")
 source_clip = VideoFileClip("tests/WANO.mp4")
-timestamps = [0] + TechnicalMemeConfig("tests/sample_config.txt").timestamps + [source_clip.end]
+timestamps = [0] + config.timestamps + [source_clip.end]
 subclips = [source_clip.subclip(*timestamps[i:i+2]) for i in range(len(timestamps) - 1)]
 	
 for exponent, subclip in enumerate(subclips):
-	vfx.speedx(subclip, 1.05**exponent).write_videofile("tests/{}.mp4".format(exponent))
+	subclips[exponent] = vfx.speedx(subclip, config.multiplier**exponent)
 
-
-# memed = CompositeVideoClip(subclips)
-# memed.without_audio().write_videofile('tests/WANO but every time they say "one" it gets 5% faster.mp4')
+memed = CompositeVideoClip(subclips)
+memed.write_videofile('tests/WANO but every time they say "one" it gets 5% faster.mp4')
