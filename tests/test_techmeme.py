62fe1ac15297d67a347f7461a75fe4ee695334e8
bmintz <bmintz@users.noreply.github.com>
2017-03-24 14:25:52 -0500

    test_techmeme.py: add test for essential attributes

--- tests/test_techmeme.py
+++ tests/test_techmeme.py
@@ -13,7 +13,9 @@ from shutil import rmtree # this and tempfile should be handled
 import tempfile # by techmeme itself but not atm
 import unittest
 
+from moviepy.video.io.VideoFileClip import VideoFileClip
 from techmeme import TechnicalMeme
+from techmeme.config import TechnicalMemeConfig
 
 
 class TechnicalMemeTests(unittest.TestCase):
@@ -21,6 +23,14 @@ class TechnicalMemeTests(unittest.TestCase):
 		# make a temp dir for storing the video parts
 		self.tmpdir = tempfile.mkdtemp(prefix="techmeme")
 		self.meme = TechnicalMeme("WANO.mp4", "sample_config.txt")
-		
+	
 	def tearDown(self):
 		rmtree(self.tmpdir)
+	
+	def test_attributes(self):
+		self.assertIsInstance(self.meme.source_video, VideoFileClip)
+		self.assertIsInstance(self.meme.config, TechnicalMemeConfig)
+	
+	def test_fix_up_timestamps(self):
+		self.assertEqual(self.meme.config.timestamps[0], 0)
+		self.assertEqual(self.meme.config.timestamps[-1], self.meme.source_video.end)
